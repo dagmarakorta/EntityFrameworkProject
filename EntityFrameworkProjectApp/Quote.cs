e16f6af7d00455e4ad02c652043b8b790b7ef8db
Dagmara Korta <dagmara.korta@banqsoft.pl>
2023-04-03 14:26:45 +0200

    Added Samurai and Quote classes

--- EntityFrameworkProjectApp/Quote.cs
+++ EntityFrameworkProjectApp/Quote.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace EntityFrameworkProjectApp.Domain
 {
-    internal class Quote
+    public class Quote
     {
+        public string? Id { get; set; }
+        public string? Text { get; set; }
+        public Samurai Samurai { get; set; } = new Samurai();
+
+        public int? SamuraiId { get; set; }
     }
 }
